1e3d26b3e2f2469b46d03a5f3fee4ec3c0d7ae8f
omerzaheer-dev <umer.zaheer341@gmail.com>
2024-08-26 00:10:04 +0500

    dood

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -32,12 +32,32 @@ const Orders = () => {
       }
       {
         order[0] && (
-          <div>
+          <div className='p-4'>
             {
               order.map((item,index)=>{
                 return(
                   <div key={index}>
                     <p>{moment(item.createdAt).format('ll')}</p>
+                    <div className='border-slate-300 border-[3px] py-1'>
+                      {
+                        item?.productDetails.map((a,i)=>{
+                          return (
+                            <div className="flex items-start gap-4 px-2 py-1">
+                              <div className='max-w-[100px] border-white min-w-[58px] min-h-[92px] flex items-center justify-between max-h-[170px] bg-slate-200'>
+                                <img className='max-w-full max-h-full w-auto h-auto mix-blend-multiply' src={a.image[0]} alt="" />
+                              </div>
+                              <div>
+                                <p className='mb-[6px] font-semibold'>{a.productName}</p>
+                                <div className='flex items-center font-semibold justify-center gap-6'>
+                                  <p className='text-red-600'>PK {a.price}</p>
+                                  <p>Quantity: {a.quantity}</p>
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
                   </div>
                 )
               })
